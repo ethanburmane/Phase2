444d70587388c42915de9da86eab5c17526decbf
Jonah Nichols <jonah@pal-nat186-11-41.itap.purdue.edu>
2023-10-26 14:21:06 -0400

    Added headers to each endpoint

---
+++ src/AWS/Lambda/package/byName/name/get.ts
@@ -0,0 +1,6 @@
+/**
+ * This file hosts the code for executing the code for GET host/package/byName
+ * 
+ * This Lambda function should return the package given by name.
+ * 
+ */
---
+++ src/AWS/Lambda/packages/post.ts
@@ -0,0 +1,6 @@
+/**
+ * This file hosts the code for executing the code for POST host/packages
+ * 
+ * This Lambda function should return all of the packages in the registry.
+ * 
+ */
