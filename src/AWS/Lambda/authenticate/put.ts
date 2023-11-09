4f790292fcc387aa1ae0f6be85f6cb695857d51c
Ashwin Sreedhar <ashwin.sreedhar2003@gmail.com>
2023-11-09 10:43:13 -0500

    Implemented user auth
    I added:
    - Input request handling
    - error handling
    - Bearer Token creation (inc. password hashing)
    - Response creation

--- src/AWS/Lambda/authenticate/put.ts
+++ src/AWS/Lambda/authenticate/put.ts
@@ -4,20 +4,41 @@
  * This Lambda function should create an access token. When a user passes in a username and secret
  *
  */
-
+import * as crypto from 'crypto';
 export const handler = async (event: any) => {
   // TODO implement
-  //Security team needs to implement this code
 
   //Process event for valid user credentials (check against registered users)
+  try {
+    const user = event.User?.name;
+    const isAdmin = event.User?.isAdmin;
+    const password = event.User?.password;
 
-  //Generate bearer token
-
+    // Incorrect AuthenticationRequest Handling
+    if (!user || typeof user !== 'string' || !password || typeof password !== 'string') {
+      return {
+        statusCode: 400,
+        body: JSON.stringify('There is missing field(s) in the AuthenticationRequest or it is formed improperly.'),
+      };
+    }
+  
+  // Create response
+  const hashedPassword = crypto.createHash('sha256').update(password).digest('hex');
+  const bearerToken = 'bearer ${user}.${hashedPassword}';   //Generate bearer token
+  
+  // Send response
+  // Return code 200 and bearer token if successful
   const response = {
     statusCode: 200,
-    body: JSON.stringify('Hello from Lambda!')
-    //token: bearer_token
-  }
+    body: JSON.stringify({token : bearerToken}),
+  };
   console.log(event)
-  return response
+  return response;
+  } catch (error) {
+    console.log(error)
+    return {
+      statusCode: 500,
+      body: JSON.stringify('There was an error with the request.'),
+    };
+  }
 }
