f96143da5eb4337601759aa0c9ae5f5d2861f2be
Ashwin Sreedhar <ashwin.sreedhar2003@gmail.com>
2023-11-09 10:35:00 -0500

    Added user authentication to post.ts
    Added:
    - input request parsing
    - error handling
    - bearer token creation

--- src/AWS/Lambda/package/post.ts
+++ src/AWS/Lambda/package/post.ts
@@ -5,28 +5,46 @@
  *
  *
  */
-
+import * as crypto from 'crypto';
 export const handler = async (event: any) => {
   // TODO implement
-
   //Authenticate Credentials
 
-  //Validate request
-
-  // - Handle validation failed
-
-  // - Handle validation success
-
   // -- Upload package into the registry
-
   // -- Update DB with information
 
-  //Create response
+  // Parse the body of the request
+  // Validate request
+  try {
+    const user = event.User?.name;
+    const isAdmin = event.User?.isAdmin;
+    const password = event.User?.password;
 
+    // - Handle validation failed
+    if (!user || typeof user !== 'string' || !password || typeof password !== 'string') {
+      return {
+        statusCode: 400,
+        body: JSON.stringify('There is missing field(s) in the AuthenticationRequest or it is formed improperly.'),
+      };
+    }
+  
+  // Create response
+  // - Handle validation success
+  const hashedPassword = crypto.createHash('sha256').update(password).digest('hex');
+  const bearerToken = 'bearer ${user}.${hashedPassword}';
+  
+  // Send response
   const response = {
     statusCode: 200,
-    body: JSON.stringify('Hello from Lambda!'),
-  }
+    body: JSON.stringify({token : bearerToken}),
+  };
   console.log(event)
-  return response
+  return response;
+} catch (error) {
+  console.log(error)
+  return {
+    statusCode: 500,
+    body: JSON.stringify('There was an error with the request.'),
+  };
+}
 }
