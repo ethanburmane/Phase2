68365f2ab7657ab46d641c4776856418a50cd197
Jonah Nichols <jonah@Jonahs-MBP.lan>
2023-11-21 18:54:13 -0500

    Updated package ingestion endpoint

--- src/AWS/Lambda/package/post.ts
+++ src/AWS/Lambda/package/post.ts
@@ -6,39 +6,50 @@
  *
  */
 
-export const handler = async (event: any) => {
-  // TODO implement
 
-  //Authenticate Credentials
+const MIN_PKG_SCORE = 0.5
 
-  //Validate request
-  const headers = event.headers
-  const authorizationHeader = headers["X-Authorization"]
-
-  const body = JSON.parse(event.body)
-  if ((body["Content"] && body["URL"]) || (!body["Content"] && !body["URL"]))
-  {
-    //Return 4xx code since the body is not formatted correctly
-  }
-  
-  //const url = extractURLFromBody(body)
-
-  
-
-
-  // - Handle validation failed
-  // - Handle validation success
-
-  // -- Upload package into the registry
-
-  // -- Update DB with information
-
-  //Create response
-
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify('Hello from Lambda!'),
-  }
+export const handler = async (event: any, context: any) => {
+  let response
   console.log(event)
+  const headers = event.headers
+  const body = event.body
+
+  // Validate request
+  if ((body.Content && body.URL) || (!body.Content && !body.URL)) {
+    // Return 4xx code since the body is not formatted correctly
+    response = {
+      statusCode: 400,
+      body: {
+        error: 'Invalid request format.',
+      },
+    }
+    return response
+  }
+  // Need the url for the package
+  let url
+  if (body.Content) {
+    url = extractUrlFromContent(body.Content)
+  } else {
+    url = body.URL
+  }
+
+  // Need to score the package
+  const score = calculateNetScore(url)
+  if (score > MIN_PKG_SCORE) {
+    response = {
+      statusCode: 200,
+      body: JSON.stringify('Hello from Lambda!'),
+    }
+  } else {
+    response = {
+      statusCode: 424,
+      body: {
+        error: 'Package rating was not high enough',
+        score: score
+      },
+    }
+  }
+
   return response
 }
