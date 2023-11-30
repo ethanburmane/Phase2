d0b43f841290b6e49a435257f40891060ec6d141
Ashwin Sreedhar <ashwin.sreedhar2003@gmail.com>
2023-11-29 20:59:31 -0500

    Initial code for GET command

--- src/AWS/Lambda/package/id/get.ts
+++ src/AWS/Lambda/package/id/get.ts
@@ -5,39 +5,87 @@
  *
  */
 
-import { S3Client, GetObjectCommand, GetObjectLegalHoldCommand } from "@aws-sdk/client-s3"; // ES Modules import
 
-const config = {}
-const s3_client = new S3Client(config)
+const { S3Client, PutObjectCommand } = require('@aws-sdk/client-s3');
+const { DynamoDBClient, PutItemCommand } = require('@aws-sdk/client-dynamodb');
 
+const AWS_REGION = "us-east-2";
 
-export const handler = async (event: any) => {
+export const handler = async (event: any, context: any) => {
+  const s3Client = new S3Client({ region: AWS_REGION });
+  const dynamoDBClient = new DynamoDBClient({ region: AWS_REGION });
+  let response;
+  console.log(event);
   // TODO implement
+  //Extract id
+  const packageId = event.queryStringParameters.id;
+  const bearerToken = event.headers.Authorization;
 
   //Validate bearer token --- SECURITY TEAM
-
-  //Extract id
-
-  // - Handle id DNE
+  if (!isValidToken(bearerToken)) {
+    response = {
+        statusCode: 400,
+        body: JSON.stringify({
+            error: 'Invalid Authentication Token'
+        }),
+    };
+    return response;
+  }
+  
 
   // - Handle id exists
-
   // -- Grab the package with the given id
-
   //Create response with package data
+  try {
+    const packageMetadata = await getPackageMetadata(packageId, dynamoDBClient);
+    if (!packageMetadata) {
+        response = {
+            statusCode: 404,
+            body: JSON.stringify({
+                error: 'Package does not exist'
+            }),
+        };
+        return response;
+    }
 
-  const target = "test.zip"
-  const s3_cmd_input = {
-    Bucket: "main-storage-bucket", // required
-    Key: "packages/test/" + target // required
-  }
-  const command = new GetObjectCommand(s3_cmd_input)
-  const r = await s3_client.send(command)
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify('Hello from Lambda!'),
-    package_content: r.Body
-  }
-  console.log(event)
-  return response
+    const s3Response = await s3Client.send(new GetObjectCommand({
+        Bucket: "main-storage-bucket",
+        Key: `packages/${packageId}/content.zip`
+    }));
+
+    response = {
+        statusCode: 200,
+        body: JSON.stringify({
+            metadata: packageMetadata,
+            data: {
+                Content: s3Response.Body.toString('base64'),
+                JSProgram: "<your_js_program_here>" // Replace with actual JS program
+            }
+        })
+    };
+} catch (error) {
+    console.error(error);
+    response = {
+        statusCode: 400,
+        body: JSON.stringify({
+            error: 'There was an error processing your request'
+        }),
+    };
+}
+
+return response;
+}
+
+function isValidToken(token: string) {
+  // Implement your token validation logic here
+  return true; // Placeholder
+}
+
+async function getPackageMetadata(packageId: string, dynamoDBClient: string) {
+  const params = {
+      TableName: "<your_dynamodb_table>",
+      Key: { 'ID': { S: packageId } }
+  };
+  const { Item } = await dynamoDBClient.send(new GetItemCommand(params));
+  return Item ? AWS.DynamoDB.Converter.unmarshall(Item) : null;
 }
