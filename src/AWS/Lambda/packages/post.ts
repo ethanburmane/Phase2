d7242438af3c24b53e58ba3818bf3dd4210fbf48
Jonah Nichols <jonah@Jonahs-MacBook-Pro.local>
2023-12-05 22:09:03 -0500

    Finished directory search

--- src/AWS/Lambda/packages/post.ts
+++ src/AWS/Lambda/packages/post.ts
@@ -5,7 +5,7 @@
  *
  */
 
-const { DynamoDBClient, BatchGetItemCommand, ScanCommand } = require("@aws-sdk/client-dynamodb")
+const { DynamoDBClient, QueryCommand, ScanCommand } = require("@aws-sdk/client-dynamodb")
 const AWS_REGION = "us-east-2"
 const DB_TABLE_NAME = "Packages"
 const DB = new DynamoDBClient({ region: AWS_REGION })
@@ -27,10 +27,10 @@ export const handler = async (event: any) => {
     }
   }
 
-  //const pkgQuery: any[] = extractQueryFromEvent(event)
-  const pkgQuery = [{ Name: "*" }]
+  const offset = extractOffset(event)
+  const pkgQuery: any[] = extractQueryFromEvent(event)
 
-  const searchResult = await performDBQuery(pkgQuery)
+  const searchResult = await performDBQuery(pkgQuery, offset)
 
   if (searchResult === false)
   {
@@ -46,7 +46,7 @@ export const handler = async (event: any) => {
   response = {
     statusCode: 200,
     body: {
-      items: items
+      packages: items
     }
   }
   return response
@@ -89,57 +89,218 @@ function extractQueryFromEvent(event: any) : any[]
   return event.body
 }
 
-async function performDBQuery(query: any[])
+async function performDBQuery(query: any[], offset: number)
 {
-  if (query[0].Name === "*")
-  {
-    const scanResult = await scanDB()
-    if (scanResult === false)
-    {
-      return false
-    }
-
-    const items = scanResult.Items
-
-    return items
-  }
-  return false
-
-  // if (query.length == 1)
-  // {
-  //   //fullDBSearch
-  // }
-  // else 
-  // {
-
-  // }
+  return await scanDB(query, offset)
 }
 
-async function scanDB()
+async function scanDB(query: any[], offset: number)
 {
-  let exclusiveStartKey = null
-   const scanParams = {
-       TableName: DB_TABLE_NAME,
-       ExclusiveStartKey: exclusiveStartKey
-   };
- 
-   const scanResult = await DB.send(new ScanCommand(scanParams))
-   if (scanResult.$metadata.httpStatusCode !== 200) {return false}
+  let scanResult
+  if (query[0] === "*")
+  {
+    scanResult = await fullDBScan(offset)
+  }
+  else 
+  {
+    scanResult = await partialDBScan(query)
+  }
 
-  // TODO Log deletion success
   return scanResult
 }
 
-function formatScanItems(scanItems: any[])
+async function partialDBScan(query: any[])
+{
+  let items: any[] = []
+  await query.forEach(async (pkg: any) =>
+  {  const params = {
+      TableName: DB_TABLE_NAME,
+      KeyConditionExpression: '#N = :n',
+      ExpressionAttributeNames: {
+        '#N': 'Name' // Replace 'Name' with your actual attribute name
+      },
+      ExpressionAttributeValues: {
+        ':n': { S: pkg.Name } // Replace 'S' with the appropriate data type of the attribute
+      }
+    };
+
+    const command = new QueryCommand(params);
+    const result = await DB.send(command);
+    if (result.$metadata.httpStatusCode === 200 && result.Items)
+    {
+      let filtered = filterQuery(result.Items, pkg.Version)
+      filtered.forEach((item: any) =>
+      {
+        items.push(item)
+      })
+    }
+    else 
+    {
+      return false
+    }
+  })
+
+  return items;
+}
+
+function filterQuery(items: any[], versionExp: string)
+{
+  let filtered: any[] = []
+  items.forEach((item: any) =>
+  {
+    if (versionMatch(versionExp, item))
+    {
+      filtered.push(item)
+    }
+  })
+  return filtered
+}
+
+function versionMatch(exp: string, item: any) : boolean
+{
+  const exactVersionRegex = /^\(?(\d+)\.(\d+)\.(\d+)\)?$/;
+  const boundedRangeRegex = /^(\()?\d+\.\d+\.\d+(-\d+\.\d+\.\d+)?(\))?$/;
+  const caretRegex = /^\^(\d+)\.(\d+)\.(\d+)$/;
+  const tildeRegex = /^~(\d+)\.(\d+)\.(\d+)$/;
+
+  if (exactVersionRegex.test(exp))
+  {
+    return isExactVersion(item.Version, exp)
+  }
+  else if (boundedRangeRegex.test(exp))
+  {
+    return isWithinBoundedRange(item.Version, exp)
+  }
+  else if (caretRegex.test(exp))
+  {
+    return isCaretVersion(item.Version, exp)
+  }
+  else if (tildeRegex.test(exp))
+  {
+    return isTildeVersion(item.Version, exp)
+  }
+
+  return false
+}
+
+function isExactVersion(version: string, targetVersion: string): boolean 
+{
+  return version === targetVersion;
+}
+
+function isCaretVersion(version: string, caretRange: string): boolean 
+{
+  const caretRegex = /^\^(\d+)\.(\d+)\.(\d+)$/;
+
+  const [, major, minor, patch] = caretRange.match(caretRegex) || [];
+  const [vMajor, vMinor, vPatch] = version.split('.').map(Number);
+
+  return (
+    vMajor === Number(major) &&
+    vMinor >= Number(minor) &&
+    (vMinor > Number(minor) || vPatch >= Number(patch))
+  );
+}
+
+function isWithinBoundedRange(version: string, range: string): boolean 
+{
+  const boundedRangeRegex = /^(\()?\d+\.\d+\.\d+(-\d+\.\d+\.\d+)?(\))?$/;
+
+  const [startRange, endRange] = range.split('-').map(v => v.replace(/[()]/g, ''));
+
+  const isGreaterThanOrEqualToStart = compareVersions(version, startRange) >= 0;
+  const isLessThanOrEqualToEnd = compareVersions(version, endRange || startRange) <= 0;
+
+  return isGreaterThanOrEqualToStart && isLessThanOrEqualToEnd;
+}
+
+function compareVersions(v1: string, v2: string): number
+{
+  const s1 = v1.split('.').map(Number);
+  const s2 = v2.split('.').map(Number);
+
+  for (let i = 0; i < Math.max(s1.length, s2.length); i++) {
+    const n1 = s1[i] || 0;
+    const n2 = s2[i] || 0;
+    if (n1 !== n2) {
+      return n1 - n2;
+    }
+  }
+  return 0;
+}
+
+function isTildeVersion(version: string, tildeRange: string): boolean
+{
+  const tildeRegex = /^~(\d+)\.(\d+)\.(\d+)$/;
+
+  const [, major, minor, patch] = tildeRange.match(tildeRegex) || [];
+  const [vMajor, vMinor, vPatch] = version.split('.').map(Number);
+
+  return (
+    vMajor === Number(major) &&
+    vMinor === Number(minor) &&
+    vPatch >= Number(patch)
+  );
+}
+
+
+
+async function fullDBScan(offset: number)
+{
+  const pageSize = 100;
+
+  let lastEvaluatedKey = null;
+  let items: any[] = [];
+
+  for (let i = 0; i < offset; i++) {
+    const params = {
+      TableName: DB_TABLE_NAME,
+      Limit: pageSize,
+      ExclusiveStartKey: lastEvaluatedKey
+    };
+
+    const command = new ScanCommand(params);
+    const dbScanResult = await DB.send(command);
+    console.log("Scan Result\n", dbScanResult)
+    if (dbScanResult.$metadata.httpStatusCode !== 200) {return false}
+    
+    const lastKey: any = dbScanResult.LastEvaluatedKey
+    const scanItems = dbScanResult.Items
+
+
+    if (lastKey === undefined || lastKey === null) {
+      // No more items to read, break the loop
+      items = scanItems;
+      break;
+    }
+
+    lastEvaluatedKey = lastKey;
+    items = scanItems;
+  }
+
+  return items;
+}
+
+function formatScanItems(scanItems: any)
 {
   let items: any[] = []
   scanItems.forEach((item: any) =>
   {
     // Need name and version
     items.push({
-      Name: item.Name,
-      Version: item.Version
+      Name: item.Name.S,
+      Version: item.Version.S,
+      id: item.id.S
     })
   })
   return items
+}
+
+function extractOffset(event: any)
+{
+  if (event.queryStringParameters && event.queryStringParameters.offset)
+  {
+    return event.queryStringParameters.offset
+  }
+  return false
 }
