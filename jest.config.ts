86ef611e8a954a0ae37779e776f7fcc26997e38e
Jonah Nichols <jonah@Jonahs-MacBook-Pro.local>
2023-12-13 14:19:02 -0500

    Jest ignore

--- jest.config.ts
+++ jest.config.ts
@@ -161,7 +161,7 @@ const config: Config = {
   // ],
 
   // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
-  testPathIgnorePatterns: ['/src/middleware/cloned-repos/'],
+  testPathIgnorePatterns: ['/src/middleware/cloned-repos/', '/src/AWS/Lambda/package/id/rate/test.js'],
 
   // The regexp pattern or array of patterns that Jest uses to detect test files
   // testRegex: [],
